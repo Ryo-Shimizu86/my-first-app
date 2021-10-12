448b7df089b9deb5a9eccc0ec9a5b9832e303541
清水良 <shimizu@1st-link.co.jp>
2021-10-12 13:40:30 +0900

    routerLinkが動作していない状態

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { MyProductListComponent } from './my-product-list/my-product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductListComponent,
+    MyProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
